4dc77b5be7b6dcb62880a34ecd259226b9d06196
Tiago Ferreira <tiferreira12@gmail.com>
2019-11-01 20:07:16 -0300

    install bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
-require('popper.js')
-require('bootstrap')
+require("bootstrap")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +16,7 @@ require('bootstrap')
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+
+
