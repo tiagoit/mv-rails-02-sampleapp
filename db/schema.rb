42dbbe062c16a99a904b17057364349d03886c68
Tiago Ferreira <tiferreira12@gmail.com>
2019-11-01 13:48:32 -0300

    add remember me

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_164841) do
+ActiveRecord::Schema.define(version: 2019_11_01_145233) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_164841) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_digest"
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,8 +17,22 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   include ApplicationHelper
 
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
   # Returns true if a test user is logged in.
   def logged_in_for_test?
     !session[:user_id].nil?
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end
